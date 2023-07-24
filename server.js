ae2e16edc2f92f1f7bf508a870ed383a4445207d
Harshita More <harshitamore28@gmail.com>
2023-07-24 10:08:09 +0530

    changed time

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require('moment-timezone');
 moment.tz.setDefault("Asia/Calcutta");
 
 function startGIF(cb){
-  const time = "2023-07-24T12:00";
+  const time = "2023-07-24T10:20";
   const width = 180;
   const height = 50;
   const color = "#3355AB";
